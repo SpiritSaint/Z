4e080bd76fe5b2c41125fc0c87fc53a70671bf7c
Ian Torres <iantorres@outlook.com>
2021-10-11 18:22:02 -0300

    Location model and migration added.

---
+++ database/migrations/2021_10_11_211214_create_locations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->string('uuid')->index()->unique();
+            $table->string('device_uuid')->index()->unique();
+            $table->string('geofence_uuid')->index()->nullable();
+            $table->string('event');
+            $table->boolean('is_moving');
+            $table->point('coords');
+            $table->double('altitude');
+            $table->double('accuracy');
+            $table->double('heading');
+            $table->double('speed');
+            $table->string('activity_type');
+            $table->double('activity_confidence');
+            $table->double('battery_level');
+            $table->boolean('battery_is_charging');
+            $table->double('odometer');
+            $table->timestamp('timestamp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
