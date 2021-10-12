317341b8a1a687e8547c42763d169e53ddd4d97c
Ian Torres <iantorres@outlook.com>
2021-10-12 15:20:23 -0300

    Migrations added.

---
+++ database/migrations/2020_05_21_200000_create_team_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('team_uuid')->index();
+            $table->string('user_uuid')->index();
+            $table->string('role')->nullable();
+            $table->timestamps();
+
+            $table->unique(['team_uuid', 'user_uuid']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_user');
+    }
+}
